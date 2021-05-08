1dbb966f3f88ab936e7efced7c31d345aa9de932
Shivam Mishra <shivam07.mt@gmail.com>
2021-05-08 15:51:12 +0530

    added logo and form inputs for signup

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,5 +1,38 @@
 import React from "react"
 
 export default function SignUp() {
-    return (<p>Hello from signup</p>)
+    return (
+        <div className="container mx-auto max-w-screen h-screen flex flex-col items-center justify-center">
+            <div className="flex w-1/4 flex-col items-center bg-white p-4 border mb-4">
+                <h1 className="flex justify-center w-full">
+                    <img src="/images/logo.png" alt='instagram logo' className="mt-2 w-6/12 mb-4" />
+                </h1>
+
+                <form method='POST'>
+                    <input aria-label="Enter your user name"
+                        type="text"
+                        placeholder="Username"
+                        className="text-sm w-full mr-3 py-5 px-4 h-2 border outline-none rounded mb-2"
+                    />
+                    <input aria-label="Enter your full name"
+                        type="text"
+                        placeholder="Full name"
+                        className="text-sm w-full mr-3 py-5 px-4 h-2 border outline-none rounded mb-2"
+                    />
+                    <input aria-label="Enter your email address"
+                        type="text"
+                        placeholder="Email address"
+                        className="text-sm w-full mr-3 py-5 px-4 h-2 border outline-none rounded mb-2"
+                    />
+
+                    <input aria-label="Enter password"
+                        type="password"
+                        placeholder="Password"
+                        className="text-sm w-full mr-3 py-5 px-4 h-2 border outline-none rounded mb-2"
+                    />
+
+                </form>
+            </div>
+        </div>
+    )
 }

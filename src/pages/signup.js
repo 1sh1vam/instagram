cc8d8e0d24abe659bb34bd7dfdaf0ef7c1b98e75
Shivam Mishra <shivam07.mt@gmail.com>
2021-05-08 16:31:58 +0530

    changed the dimensions of form

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -5,25 +5,29 @@ import * as ROUTES from "../constants/routes"
 export default function SignUp() {
     return (
         <div className="container mx-auto max-w-screen h-screen flex flex-col items-center justify-center">
-            <div className="flex w-1/4 flex-col items-center bg-white p-4 border mb-4">
+            <div className="flex w-1/4 flex-col items-center justify-center bg-white p-4 border mb-4">
                 <h1 className="flex justify-center w-full">
-                    <img src="/images/logo.png" alt='instagram logo' className="mt-2 w-6/12 mb-4" />
+                    <img src="/images/logo.png" alt='instagram logo' className="mt-5 w-6/12 mb-4" />
                 </h1>
 
-                <form method='POST'>
+                <h3 className="w-4/5 text-center text-lg font-semibold text-gray-400 mb-4">Sign up to see photos and videos from your friends.</h3>
+
+                <form method='POST' className="w-4/5">
+                    <input aria-label="Enter your email address"
+                        type="text"
+                        placeholder="Mobile Number or Email"
+                        className="text-sm w-full mr-3 py-5 px-4 h-2 border outline-none rounded mb-2"
+                    />
+                    
+                    <input aria-label="Enter your full name"
+                        type="text"
+                        placeholder="Full Name"
+                        className="text-sm w-full mr-3 py-5 px-4 h-2 border outline-none rounded mb-2"
+                    />
+
                     <input aria-label="Enter your user name"
                         type="text"
                         placeholder="Username"
-                        className="text-sm w-full mr-3 py-5 px-4 h-2 border outline-none rounded mb-2"
-                    />
-                    <input aria-label="Enter your full name"
-                        type="text"
-                        placeholder="Full name"
-                        className="text-sm w-full mr-3 py-5 px-4 h-2 border outline-none rounded mb-2"
-                    />
-                    <input aria-label="Enter your email address"
-                        type="text"
-                        placeholder="Email address"
                         className="text-sm w-full mr-3 py-5 px-4 h-2 border outline-none rounded mb-2"
                     />
 

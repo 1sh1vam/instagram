10ac5f6fce4f50e7d5472fa6f3281a3fc5cc0d4b
Shivam Mishra <shivam07.mt@gmail.com>
2021-05-16 10:50:28 +0530

    setting up dashboard page

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,5 +1,16 @@
 import React from "react"
+import Header from "../components/header.js"
+import Sidebar from "../components/sidebar/index.js"
+import Timeline from "../components/timeline.js"
 
 export default function Dashboard() {
-    return (<p>Hello from Dashboard</p>)
+    return (
+        <div className="bg-gray-100">
+            <p>Hello from Dashboard</p>
+            <div className="">
+                <Header />
+                <Timeline />
+            </div>
+        </div>
+    )
 }

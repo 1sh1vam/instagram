f54b2af1eda92af563541946898356fbebd8cd3a
Shivam Mishra <shivam07.mt@gmail.com>
2021-05-29 12:56:32 +0530

    added usUser hook

---
+++ src/hooks/use-user.js
@@ -0,0 +1,20 @@
+import React, { useState, useContext, useEffect } from "react"
+import UserContext from "../context/user"
+
+export default function useUser() {
+    const [activeUser, setActiveUser] = useState()
+    const { user } = useContext(UserContext)
+
+    useEffect(() => {
+        async function getUserObjByUserId() {
+            const [response] = await getUserByUserId(user.uid)
+        }
+        if(user && user.uid) {
+            getUserObjByUserId()
+        }
+    }, [user])
+
+    return {
+        user: activeUser
+    }
+}

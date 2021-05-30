a6934759c03a8592f8d0d3f2da1c5098e8f4a8ec
Shivam Mishra <shivam07.mt@gmail.com>
2021-05-30 12:34:48 +0530

    added useFollowedUserPhotos hooks in timeline

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,8 +1,18 @@
 import React from "react"
 import Skeleton from "react-loading-skeleton"
+import useFollowedUsersPhotos from "../hooks/use-followed-users-photos"
 
 export default function Timeline() {
+    const { photos } = useFollowedUsersPhotos()
+
     return (
-        <Skeleton count={4} width={640} height={500} className="mb-5"/>
+        <div>
+            {!photos ? (
+                <Skeleton count={4} width={640} height={500} className="mb-5"/>
+            )
+            :(
+                photos.map((photo) => "<p>I am the photo</p>")
+            )}
+        </div>
     )
 }

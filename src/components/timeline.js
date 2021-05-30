e44e55393968a427cbaf97cfb81237813d1964ff
Shivam Mishra <shivam07.mt@gmail.com>
2021-05-30 12:18:17 +0530

    added react loading skeleton

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,5 +1,8 @@
 import React from "react"
+import Skeleton from "react-loading-skeleton"
 
 export default function Timeline() {
-    return (<p>I am from timeline</p>)
+    return (
+        <Skeleton count={4} width={640} height={500} className="mb-5"/>
+    )
 }
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -4,10 +4,11 @@ import Sidebar from "../components/sidebar/index.js"
 import Timeline from "../components/timeline.js"
 
 export default function Dashboard() {
+
     return (
         <div className="bg-insta-gray">
             <Header />
-            <div className="grid grid-cols-3 gap-4 justify-between max-w-screen-lg mx-auto">
+            <div className="grid grid-cols-3 gap-4 justify-between maxw-975 mx-auto">
                 <Timeline />
                 <Sidebar/>
             </div>

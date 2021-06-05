1b86ed19eb2064c8a26df230805ecfdfa19a4cce
Shivam Mishra <shivam07.mt@gmail.com>
2021-06-05 12:57:10 +0530

    setting up the footer

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Image from './image';
+import Footer from './footer';
 
 export default function Post({ content }) {
 
     return (
         <div className="rounded mb-16 bg-white border">
             <Image src={content.imageSrc} caption={content.caption} />
+            <Footer caption={content.caption} username={content.username} />
         </div>
     )
 }

b8cd8fc423bac002bee99b5de4b6319b0b8a2117
Shivam Mishra <shivam07.mt@gmail.com>
2021-06-05 13:43:44 +0530

    adding the color togglr in the post

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import React,{ lazy, Suspense } from "react"
+import React, { lazy, Suspense } from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import UserContext from "./context/user"
 import useAuthListener from "./hooks/use-auth-listener"
 import * as ROUTES from "./constants/routes"
 
-const Login = lazy(()=> import("./pages/login"));
-const SignUp = lazy(()=> import("./pages/signup"));
-const Profile = lazy(()=> import("./pages/profile"));
-const NotFound = lazy(()=> import("./pages/not-found"));
-const Dashboard = lazy(()=> import("./pages/dashboard"));
+const Login = lazy(() => import("./pages/login"));
+const SignUp = lazy(() => import("./pages/signup"));
+const Profile = lazy(() => import("./pages/profile"));
+const NotFound = lazy(() => import("./pages/not-found"));
+const Dashboard = lazy(() => import("./pages/dashboard"));
 
 function App() {
   const { user } = useAuthListener()
   return (
-    <UserContext.Provider value={{user}}>
-        <Router>
-          <Suspense fallback={<p>Loading...</p>}>
-            <Switch>
-              <Route path={ROUTES.LOGIN} component={Login} />
-              <Route path={ROUTES.SIGN_UP} component={SignUp} />
-              <Route path={ROUTES.PROFILE} component={Profile} />
-              <Route path={ROUTES.DASHBOARD} component={Dashboard} exact />
-              <Route component={NotFound} />
-            </Switch>
-          </Suspense>
-        </Router>
+    <UserContext.Provider value={{ user }}>
+      <Router>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Switch>
+            <Route path={ROUTES.LOGIN} component={Login} />
+            <Route path={ROUTES.SIGN_UP} component={SignUp} />
+            <Route path={ROUTES.PROFILE} component={Profile} />
+            <Route path={ROUTES.DASHBOARD} component={Dashboard} exact />
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
+      </Router>
     </UserContext.Provider>
   );
 }
--- src/hooks/use-followed-users-photos.js
+++ src/hooks/use-followed-users-photos.js
@@ -4,13 +4,17 @@ import { getUserByUserId, getUserFollowedPhotos } from "../services/firebase"
 
 export default function useFollowedUsersPhotos() {
     const [photos, setPhotos] = useState(null)
-    const { user: {uid: userId = ""}} = useContext(UserContext)
+    const { user } = useContext(UserContext);
+    const userId = user ? user.uid : '';
+    console.log(userId)
 
     useEffect(() => {
         async function getTimelinePhotos() {
             const followingUserIds = await getUserByUserId(userId)
 
-            if(followingUserIds && followingUserIds[0].following.length > 0) {
+            if (followingUserIds
+                && followingUserIds.length > 0
+                && followingUserIds[0].following.length > 0) {
                 const followedUserPhotos = await getUserFollowedPhotos(userId, followingUserIds[0].following)
                 followedUserPhotos.sort((a, b) => b.dateCreated - a.dateCreated)
                 setPhotos(followedUserPhotos)

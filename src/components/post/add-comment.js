df1c5f2e181e5b4363775233b339434faa686154
Shivam Mishra <shivam07.mt@gmail.com>
2021-06-06 13:40:27 +0530

    adding the comment functionality

--- src/components/post/add-comment.js
+++ src/components/post/add-comment.js
@@ -1,12 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import FirebaseContext from '../../context/firebase';
+import UserContext from '../../context/user';
 
 export default function AddComment({ docId, setComments, commentInput }) {
     const [comment, setComment] = useState('');
+    const { firebase, FieldValue } = useContext(FirebaseContext);
+    const {
+        user: { displayName }
+    } = useContext(UserContext);
+
+    const handleCommentSubmit = async (event) => {
+        event.preventDefault();
+        setComments(prev => [{ comment, displayName }, ...prev]);
+        setComment('');
+
+        console.log(comment)
+
+        return firebase
+            .firestore()
+            .collection('photos')
+            .doc(docId)
+            .update({
+                comments: FieldValue.arrayUnion({ displayName, comment })
+            })
+    }
+
 
     return (
         <div className="border-t border-gray">
             <form method="POST"
                 className="w-full flex items-center justify-between pr-5"
+                onSubmit={handleCommentSubmit}
             >
                 <input
                     type="text"
